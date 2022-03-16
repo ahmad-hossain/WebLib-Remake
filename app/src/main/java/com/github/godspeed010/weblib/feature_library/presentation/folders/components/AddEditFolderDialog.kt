d31b2b497ff4f9206ed14da556b6e1de0153c190
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-03-16 17:23:11 -0400

    improved styling for FolderDialog

--- app/src/main/java/com/github/godspeed010/weblib/feature_library/presentation/folders/components/AddEditFolderDialog.kt
+++ app/src/main/java/com/github/godspeed010/weblib/feature_library/presentation/folders/components/AddEditFolderDialog.kt
@@ -1,14 +1,17 @@
 package com.github.godspeed010.weblib.feature_library.presentation.folders.components
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.github.godspeed010.weblib.feature_library.domain.model.Folder
-import com.github.godspeed010.weblib.feature_library.presentation.folders.FoldersEvent
-import com.github.godspeed010.weblib.ui.theme.Shapes
+import androidx.compose.ui.window.Dialog
 
 @Composable
 fun AddEditFolderDialog(
@@ -19,30 +22,75 @@ fun AddEditFolderDialog(
     onDismissDialog: () -> Unit,
     onConfirmDialog: () -> Unit
 ) {
-    AlertDialog(
-        modifier = modifier,
-        shape = RoundedCornerShape(10.dp),
+    Dialog(
         onDismissRequest = onDismissDialog,
-        title = {
-            Text(title)
-        },
-        text = {
-            OutlinedTextField(
-                value = folderName,
-                onValueChange = {
-                    onTextChange(it)
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            backgroundColor = MaterialTheme.colors.surface,
+            elevation = 24.dp
+        ) {
+            Column {
+                // Title
+                Text(
+                    modifier = Modifier
+                        .firstBaselineToTop(40.dp)
+                        .padding(horizontal = 24.dp),
+                    text = title,
+                    style = MaterialTheme.typography.h6
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                // TextField
+                OutlinedTextField(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(horizontal = 24.dp),
+                    value = folderName,
+                    onValueChange = {
+                        onTextChange(it)
+                    }
+                )
+
+                Spacer(Modifier.height(10.dp))
+
+                //Submit and Cancel Buttons
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp, end = 8.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = onDismissDialog) {
+                        Text("CANCEL")
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    TextButton(onClick = onConfirmDialog) {
+                        Text("OK")
+                    }
                 }
-            )
-        },
-        confirmButton = {
-            TextButton(onClick = onConfirmDialog) {
-                Text("OK")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismissDialog) {
-                Text("CANCEL")
             }
         }
-    )
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = layout { measurable, constraints ->
+    // Measure the composable
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
 }
