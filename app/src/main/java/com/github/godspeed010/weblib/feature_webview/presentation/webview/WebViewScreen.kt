92ec16a4c675172b2d894320cefeabb3e02d5081
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-09-08 14:15:24 -0400

    - Removed Scaffold in WebViewScreen, replacing with Custom-Column layout - Set height of WebView to be screen height, enabling WebView to fill screen when TopAppBar is collapsed

--- app/src/main/java/com/github/godspeed010/weblib/feature_webview/presentation/webview/WebViewScreen.kt
+++ app/src/main/java/com/github/godspeed010/weblib/feature_webview/presentation/webview/WebViewScreen.kt
@@ -1,18 +1,21 @@
 package com.github.godspeed010.weblib.feature_webview.presentation.webview
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.godspeed010.weblib.feature_library.domain.model.Novel
+import com.github.godspeed010.weblib.feature_webview.presentation.webview.components.BasicColumn
 import com.github.godspeed010.weblib.feature_webview.presentation.webview.components.WebViewTopAppBar
 import com.github.godspeed010.weblib.feature_webview.util.CustomWebViewClient
 import com.github.godspeed010.weblib.feature_webview.util.LoadingState
@@ -33,46 +36,43 @@ fun WebViewScreen(
     viewModel: WebViewViewModel = hiltViewModel()
 ) {
     val state by viewModel.webViewScreenState
-    val scaffoldState = rememberScaffoldState()
     val localDensity = LocalDensity.current
     viewModel.observeLifecycle(LocalLifecycleOwner.current.lifecycle)
 
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            //todo remove out of scaffold. Maybe that will fix whitespace when collapsed
-            // Other possible solution: Increase WebView height while Toolbar collapses
-            WebViewTopAppBar(
-                modifier = Modifier.offset { IntOffset(0, state.toolbarOffsetHeightPx.roundToInt() ) },
-                url = state.addressBarText,
-                onUrlEntered = { viewModel.onEvent(WebViewEvent.EnteredUrl(it)) },
-                onUrlSubmitted = { viewModel.onEvent(WebViewEvent.SubmittedUrl) },
-                onBackButtonClicked = { navigator.popBackStack() },
-                onRefreshClicked = { viewModel.onEvent(WebViewEvent.ReloadClicked) },
-                onMoreOptionsClicked = { viewModel.onEvent(WebViewEvent.MoreOptionsClicked) },
+    BasicColumn {
+        WebViewTopAppBar(
+            modifier = Modifier.offset { IntOffset(0, state.toolbarOffsetHeightPx.roundToInt()) },
+            url = state.addressBarText,
+            onUrlEntered = { viewModel.onEvent(WebViewEvent.EnteredUrl(it)) },
+            onUrlSubmitted = { viewModel.onEvent(WebViewEvent.SubmittedUrl) },
+            onBackButtonClicked = { navigator.popBackStack() },
+            onRefreshClicked = { viewModel.onEvent(WebViewEvent.ReloadClicked) },
+            onMoreOptionsClicked = { viewModel.onEvent(WebViewEvent.MoreOptionsClicked) },
+        )
+
+        val loadingState = state.webViewState.loadingState
+        if (loadingState is LoadingState.Loading) {
+            LinearProgressIndicator(
+                modifier = Modifier.fillMaxWidth(),
+                progress = loadingState.progress
             )
         }
-    ) { innerPadding ->
-        Column {
-            val loadingState = state.webViewState.loadingState
-            if (loadingState is LoadingState.Loading) {
-                LinearProgressIndicator(
-                    modifier = Modifier.fillMaxWidth(),
-                    progress = loadingState.progress
+
+        WebView(
+            modifier = Modifier
+                .requiredHeight(LocalConfiguration.current.screenHeightDp.dp)
+                .offset { IntOffset(0, state.toolbarOffsetHeightPx.roundToInt()) },
+            state = state.webViewState,
+            onCreated = { it.settings.javaScriptEnabled = true },
+            navigator = state.webViewNavigator,
+            client = remember {
+                CustomWebViewClient(
+                    onNewPageVisited = { viewModel.onEvent(WebViewEvent.NewPageVisited(it)) }
                 )
+            },
+            onWebPageScroll = { x, y, oldX, oldY ->
+                viewModel.onEvent(WebViewEvent.WebPageScrolled(localDensity, x, y, oldX, oldY))
             }
-            WebView(
-                modifier = Modifier.padding(innerPadding),
-                state = state.webViewState,
-                onCreated = { it.settings.javaScriptEnabled = true },
-                navigator = state.webViewNavigator,
-                client = remember {
-                    CustomWebViewClient(
-                        onNewPageVisited = { viewModel.onEvent(WebViewEvent.NewPageVisited(it)) }
-                    )
-                },
-                onWebPageScroll = { x, y, oldX, oldY -> viewModel.onEvent(WebViewEvent.WebPageScrolled(localDensity, x, y, oldX, oldY)) }
-            )
-        }
+        )
     }
 }
