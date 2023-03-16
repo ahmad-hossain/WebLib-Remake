39b943543f5494edc5860a77ea78743ffebb187d
Name <ahmad.awaab@gmail.com>
2023-03-16 04:42:49 -0400

    M3 colors are applied to settings-screen xml layout

--- app/src/main/java/com/github/godspeed010/weblib/feature_webview/presentation/webview/WebViewScreen.kt
+++ app/src/main/java/com/github/godspeed010/weblib/feature_webview/presentation/webview/WebViewScreen.kt
@@ -2,12 +2,17 @@ package com.github.godspeed010.weblib.feature_webview.presentation.webview
 
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
+import android.webkit.WebViewClient
+import android.widget.EditText
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.github.godspeed010.weblib.R
 import com.github.godspeed010.weblib.databinding.LayoutWebViewBinding
 import com.github.godspeed010.weblib.feature_library.domain.model.Novel
 import com.github.godspeed010.weblib.observeLifecycle
@@ -27,11 +32,49 @@ fun WebViewScreen(
     viewModel.observeLifecycle(LocalLifecycleOwner.current.lifecycle)
     val state = viewModel.state
 
-    val color = MaterialTheme.colorScheme.onSurface
+    val colorScheme = MaterialTheme.colorScheme
     AndroidViewBinding(LayoutWebViewBinding::inflate) {
-        this.webviewToolbar.navigationIcon?.colorFilter = PorterDuffColorFilter(color.toArgb(), PorterDuff.Mode.SRC_IN)
+        applyMaterialTheme(colorScheme)
         webview.settings.javaScriptEnabled = true
         webview.loadUrl("https://www.google.com")
     }
+}
 
+private fun LayoutWebViewBinding.applyMaterialTheme(colorScheme: ColorScheme) {
+    // Navigation Icon
+    webviewToolbar.navigationIcon?.colorFilter =
+        PorterDuffColorFilter(colorScheme.onSurface.toArgb(), PorterDuff.Mode.SRC_IN)
+
+    // Trailing Icons
+    val menu = webviewToolbar.menu
+    val onSurfaceVariantColorFilter =
+        PorterDuffColorFilter(colorScheme.onSurfaceVariant.toArgb(), PorterDuff.Mode.SRC_IN)
+    for (i in 0 until menu.size()) {
+        menu.getItem(i).icon?.colorFilter = onSurfaceVariantColorFilter
+    }
+
+    // Toolbar SubMenu Icons
+    val subMenu = menu.findItem(R.id.more_options).subMenu
+    for (i in 0 until (subMenu?.size() ?: 0)) {
+        subMenu?.getItem(i)?.icon?.colorFilter = onSurfaceVariantColorFilter
+    }
+
+    // Address Bar Background
+    val addressBar = webviewToolbar.findViewById<EditText>(R.id.et_address_bar)
+    val roundShape = ContextCompat.getDrawable(
+        this@applyMaterialTheme.root.context,
+        R.drawable.shape_rounded_edittext
+    )
+    roundShape?.colorFilter =
+        PorterDuffColorFilter(colorScheme.surfaceVariant.toArgb(), PorterDuff.Mode.SRC_IN)
+    addressBar.background = roundShape
+
+    // Address Bar Hint
+    addressBar.setHintTextColor(colorScheme.onSurfaceVariant.toArgb())
+
+    // Address Bar Text
+    addressBar.setTextColor(colorScheme.onSurface.toArgb())
+
+    // Toolbar Container
+    webviewToolbar.setBackgroundColor(colorScheme.surface.toArgb())
 }
