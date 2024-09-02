e521284ef1280877689051033afb08546c7fc69f
Name <ahmad.awaab@gmail.com>
2024-09-01 22:06:12 -0700

    Refactored NovelDaoTest to improve readability

--- app/src/androidTest/java/com/github/godspeed010/weblib/feature_library/data/data_source/NovelDaoTest.kt
+++ app/src/androidTest/java/com/github/godspeed010/weblib/feature_library/data/data_source/NovelDaoTest.kt
@@ -44,66 +44,61 @@ class NovelDaoTest {
     fun folderDeletionDeletesChildNovels() = runTest {
         val parentFolder = Folder(1, "title")
         val remainingFolder = Folder(2, "Title")
-        folderDao.insert(parentFolder)
-        folderDao.insert(remainingFolder)
-
         val novels = listOf(
             Novel.createWithDefaults(id = 1, title = "name", url = "url", folderId = 1),
-            Novel.createWithDefaults(id = 2, title = "title",url =  "url", folderId = 1),
+            Novel.createWithDefaults(id = 2, title = "title", url = "url", folderId = 1),
             Novel.createWithDefaults(id = 3, title = "name", url = "url_Here", folderId = 1),
         )
-        val remainingNovel = Novel.createWithDefaults(id = 5, title = "name", url = "url_Here", folderId = 2)
-        novels.forEach { novelDao.insert(it) }
-        novelDao.insert(remainingNovel)
+        val remainingNovel =
+            Novel.createWithDefaults(id = 5, title = "name", url = "url_Here", folderId = 2)
 
+        folderDao.insert(parentFolder)
+        folderDao.insert(remainingFolder)
+        (novels + remainingNovel).forEach { novelDao.insert(it) }
         folderDao.delete(parentFolder)
 
-        val allNovels = novelDao.getNovels().first()
-
-        assertThat(allNovels).containsExactly(remainingNovel)
+        assertThat(getAllNovels()).containsExactly(remainingNovel)
     }
 
     @Test
     fun addNovel() = runTest {
-        //folder with same folderId must be created first due to parent-child relationship
+        // folder with same folderId must be created first due to parent-child relationship
         val parentFolder = Folder(1, "title")
-        folderDao.insert(parentFolder)
-
         val novel = Novel.createWithDefaults(id = 1, title = "title", url = "url", folderId = 1)
+
+        folderDao.insert(parentFolder)
         novelDao.insert(novel)
 
-        val allNovels = novelDao.getNovels().first()
-        assertThat(allNovels).containsExactly(novel)
+        assertThat(getAllNovels()).containsExactly(novel)
     }
 
     @Test
     fun deleteNovel() = runTest {
         val parentFolder = Folder(1, "title")
-        folderDao.insert(parentFolder)
-
         val novel = Novel.createWithDefaults(id = 1, title = "title", url = "url", folderId = 1)
-        val remainingNovel = Novel.createWithDefaults(id = 2, title = "something", url = "urlabc", folderId = 1)
+        val remainingNovel =
+            Novel.createWithDefaults(id = 2, title = "something", url = "urlabc", folderId = 1)
+
+        folderDao.insert(parentFolder)
         novelDao.insert(novel)
         novelDao.insert(remainingNovel)
         novelDao.delete(novel)
 
-        val allNovels = novelDao.getNovels().first()
-
-        assertThat(allNovels).containsExactly(remainingNovel)
+        assertThat(getAllNovels()).containsExactly(remainingNovel)
     }
 
     @Test
     fun updateNovel() = runTest {
         val parentFolder = Folder(1, "title")
-        folderDao.insert(parentFolder)
-
         val novel = Novel.createWithDefaults(id = 3, title = "title", url = "url", folderId = 1)
-        val updatedNovel = Novel.createWithDefaults(id = 3, title = "new", url = "newurl", folderId = 1)
+        val updatedNovel =
+            Novel.createWithDefaults(id = 3, title = "new", url = "newurl", folderId = 1)
+
+        folderDao.insert(parentFolder)
         novelDao.insert(novel)
         novelDao.update(updatedNovel)
 
-        val allNovels = novelDao.getNovels().first()
-        assertThat(allNovels).containsExactly(updatedNovel)
+        assertThat(getAllNovels()).containsExactly(updatedNovel)
     }
 
     @Test
@@ -114,36 +109,35 @@ class NovelDaoTest {
             Folder(8, "abcTitle"),
             Folder(6, "titleHere"),
         )
-        folders.forEach { folderDao.insert(it) }
+        val novels = listOf(
+            Novel.createWithDefaults(id = 1, title = "hello", url = "url", folderId = 9),
+            Novel.createWithDefaults(id = 2, title = "hello world", url = "url", folderId = 8),
+            Novel.createWithDefaults(id = 3, title = "title", url = "hello", folderId = 7)
+        )
+        val noMatchNovel =
+            Novel.createWithDefaults(id = 4, title = "title", url = "url", folderId = 6)
 
-        val novel1 = Novel.createWithDefaults(id = 1, title = "hello", url = "url", folderId = 9)
-        val novel2 = Novel.createWithDefaults(id = 2, title = "hello world", url = "url", folderId = 8)
-        val novel3 = Novel.createWithDefaults(id = 3, title = "title", url = "hello", folderId = 7)
-        val noMatch = Novel.createWithDefaults(id = 4, title = "title", url = "url", folderId = 6)
-        novelDao.insert(novel1)
-        novelDao.insert(novel2)
-        novelDao.insert(novel3)
-        novelDao.insert(noMatch)
+        folders.forEach { folderDao.insert(it) }
+        (novels + noMatchNovel).forEach { novelDao.insert(it) }
 
         val queriedNovels = novelDao.findNovelsByNameOrUrl("%hell%")
-
-        assertThat(queriedNovels).containsExactly(novel1, novel2, novel3)
+        assertThat(queriedNovels).containsExactlyElementsIn(novels)
     }
 
     @Test
     fun getNovels() = runTest {
         val parentFolder = Folder(id = 5, title = "title here")
-        folderDao.insert(parentFolder)
-
         val novels = listOf(
             Novel.createWithDefaults(id = 1, title = "title", url = "url", folderId = 5),
             Novel.createWithDefaults(id = 2, title = "titleabc", url = "url", folderId = 5),
             Novel.createWithDefaults(id = 3, title = "title", url = "url", folderId = 5),
         )
+
+        folderDao.insert(parentFolder)
         novels.forEach { novelDao.insert(it) }
 
-        val allNovels = novelDao.getNovels().first()
-
-        assertThat(allNovels).containsExactlyElementsIn(novels)
+        assertThat(getAllNovels()).containsExactlyElementsIn(novels)
     }
+
+    private suspend fun getAllNovels(): List<Novel> = novelDao.getNovels().first()
 }
