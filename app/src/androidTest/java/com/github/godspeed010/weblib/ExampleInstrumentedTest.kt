796b1f9d89efa5065c35208086256e0c5f3bfcd9
Ahmad <ahmad.awaab@gmail.com>
2025-12-27 19:51:03 -0800

    Fix failing ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/github/godspeed010/weblib/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/godspeed010/weblib/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.weblib", appContext.packageName)
+        assertEquals("com.github.godspeed010.weblib", appContext.packageName)
     }
 }
